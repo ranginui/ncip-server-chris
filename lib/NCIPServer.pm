51637c9b91ab800a180500bd7cc35bca63b00cdb
Chris Cormack <chrisc@catalyst.net.nz>
2013-08-28 12:07:09 +1200

    Server now starts, and uses the config
    Signed-off-by: Jason Stephenson <jstephenson@mvlc.org>

--- lib/NCIPServer.pm
+++ lib/NCIPServer.pm
@@ -1,14 +1,31 @@
 package NCIPServer;
 
 use Modern::Perl;
+use NCIP::Configuration;
 
-require Net::Server::PreFork;;
-
-our @ISA = qw(Net::Server::PreFork);
+use base qw(Net::Server::PreFork);
 
 our $VERSION = '0.01';
 
+sub configure_hook {
+    my ($self)        = @_;
+    my $server        = $self->{'server'};
+    my $config        = NCIP::Configuration->new( $server->{'config_dir'} );
+    my $server_params = $config->('NCIP.server-params');
+    while ( my ( $key, $val ) = each %$server_params ) {
+        $server->{$key} = $val;
+    }
+    my $listeners = $config->('NCIP.listeners');
+    foreach my $svc ( keys %$listeners ) {
+        $server->{'port'} = $listeners->{$svc}->{'port'};
+    }
+}
 
+sub post_configure_hook {
+    my $self = shift;
+    use Data::Dumper;
+    print Dumper $self;
+}
 
 1;
 __END__
