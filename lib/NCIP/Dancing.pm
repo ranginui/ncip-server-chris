78be35490427c3ed7de1a6b2f1ab9865aa258c9c
Chris Cormack <chrisc@catalyst.net.nz>
2013-12-30 14:42:19 +1300

    Got the dancer app working now

--- lib/NCIP/Dancing.pm
+++ lib/NCIP/Dancing.pm
@@ -3,8 +3,18 @@ use Dancer ':syntax';
 
 our $VERSION = '0.1';
 
-get '/' => sub {
-    template 'index';
+use NCIP;
+
+
+any ['get', 'post'] => '/' => sub {
+    my $ncip = NCIP->new('t/config_sample');
+    my $xml = param 'xml';
+    warn $xml if $xml;
+    my $content = $ncip->process_request($xml);
+  #  warn $content;
+    template 'main', { content => $content };
+
+  #  warn "what";
 };
 
 true;
