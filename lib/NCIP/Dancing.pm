e649244582bda6cb321abdcfbb4f8f17144a3a00
Chris Cormack <chrisc@catalyst.net.nz>
2014-01-13 13:58:39 +1300

    Tidying up dancer app

--- lib/NCIP/Dancing.pm
+++ lib/NCIP/Dancing.pm
@@ -9,12 +9,8 @@ use NCIP;
 any ['get', 'post'] => '/' => sub {
     my $ncip = NCIP->new('t/config_sample');
     my $xml = param 'xml';
-    warn $xml if $xml;
     my $content = $ncip->process_request($xml);
-  #  warn $content;
     template 'main', { content => $content };
-
-  #  warn "what";
 };
 
 true;
