b567cd0489074759da5bba0bf5201dcdd4505fb3
Chris Cormack <chrisc@catalyst.net.nz>
2014-03-20 10:07:56 +1300

    CheckinItem handler

--- lib/NCIP/Handler/CheckInItem.pm
+++ lib/NCIP/Handler/CheckInItem.pm
@@ -23,21 +23,24 @@ sub handle {
     my $self   = shift;
     my $xmldoc = shift;
     if ($xmldoc) {
-        my $root = $xmldoc->documentElement();
-        my $xpc = $self->xpc();
-        my $itemid =
-          $xpc->findnodes('//ns:ItemId',$root);
+        my $root   = $xmldoc->documentElement();
+        my $xpc    = $self->xpc();
+        my $itemid = $xpc->findnodes( '//ns:ItemIdentifierValue', $root );
 
         # checkin the item
-        my $branch='AS'; # where the hell do we get this from???
-        my $checkin = $self->ils->checkin( $itemid,$branch );
+        my $branch = 'AS';    # where the hell do we get this from???
+        my $checkin = $self->ils->checkin( $itemid, $branch );
         my $output;
         my $vars;
         $vars->{'messagetype'} = 'CheckInItemResponse';
-        $vars->{'barcode'} = $itemid;
+        $vars->{'barcode'}     = $itemid;
+        my ( $from, $to ) = $self->get_agencies($xmldoc);
+        $vars->{'fromagency'} = $to;
+        $vars->{'toagency'}   = $from;
+
         if ( !$checkin->{success} ) {
-            $vars->{'processingerror'} = 1;
-            $vars->{'processingerrortype'} = $checkin->{'messages'};
+            $vars->{'processingerror'}        = 1;
+            $vars->{'processingerrortype'}    = $checkin->{'messages'};
             $vars->{'processingerrorelement'} = 'UniqueItemIdentifier';
             $output = $self->render_output( 'problem.tt', $vars );
         }
